a0021ae18e70afa406f6e57e606d77648125d659
hezz <hezzh@yunyichina.cn>
2017-05-26 18:50:52 +0800

    Merge branch 'test' of https://github.com/MMDTechnology/WalnutAndroid into test
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/java/maojian/android/walnut/Home.java

---
+++ app/src/main/java/maojian/android/walnut/VideoPickerActivity.java
@@ -0,0 +1,13 @@
+package maojian.android.walnut;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoPickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_picker);
+    }
+}
